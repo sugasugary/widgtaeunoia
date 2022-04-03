36b8701d32cdef5ab626c8b6ee35598caa78acf5
sugasugary <95432496+sugasugary@users.noreply.github.com>
2022-04-03 17:33:29 +0700

    Add files via upload

---
+++ afffwwqwe5.js
@@ -0,0 +1,6 @@
+ document.getElementById("dload").onclick = function(){
+  var l = document.createElement("a");
+  l.href = "data:text/plain;charset=UTF-8," + document.getElementById("dload-txt").value;
+  l.setAttribute("download", document.getElementById("dload-fn").value);
+  l.click();
+}
